b7d17a31f638dbb029c0ab59e4e8addbc81e8dda
Aishwarya22222 <154801337+Aishwarya22222@users.noreply.github.com>
2023-12-28 19:57:31 +0545

    pythonproject push

---
+++ datatype.py
@@ -0,0 +1,37 @@
+#int (integer) or whole number ->12,15,-20
+#float or real number->5.2, -2.4, -5.6
+#str(string) ->'hello student'
+
+print(15)
+print(22,56)
+print('hello student')
+
+print(type(10))
+print(type('hello babbeee'))
+print(type(34.8))
+
+# list-> list contains more than one value and it is mutable that is element inside list can be changed.
+#eg:[2,5,6,7,]
+
+print(['apple','mango'])
+print(type([1,2,3,4,5]))
+
+#tuple-> similar to list but immutable that is element inside tuple cannot be changed. eg('a','b','c')
+print(type(('a','b','v')))
+
+#boolean-> True or False
+print(True)
+print(False)
+print(type(True))
+
+#dict(dictionary)-> it contains key value pair
+#eg:{'key1':"value1','key2':'value2','key3':'value3'}
+
+print({
+    'first_name':'Aishwarya',
+    'second name':'Neupane',
+    'age':24
+})
+
+#set->unorder collection of unique elements
+print(set([1,4,5,34,75,8,9,46,5,5]))
