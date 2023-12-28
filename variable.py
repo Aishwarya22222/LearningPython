f45782d29bba454e14012d64521ee17f01cd0a93
Aishwarya22222 <154801337+Aishwarya22222@users.noreply.github.com>
2023-12-28 20:25:58 +0545

    learning variable

---
+++ variable.py
@@ -0,0 +1,16 @@
+# variable is a container to store values while naming variable special character except underscore(_)
+# must be avoided, whitespace must be avoided, python inbuilt keywords and methods must be avoided, 
+# numeric value at the beginningmust be avaoided
+
+x=30
+print(x)
+
+#string formatting
+print('the value of x is:',x)
+
+first_name='Ram'
+last_name='shrestha'
+address='Pokhara'
+print('My name is',first_name,last_name,'I live in',address)
+print(f'My name is {first_name} {last_name}. I live in {address}.')
+print('my name is' + first_name+' '+last_name)
