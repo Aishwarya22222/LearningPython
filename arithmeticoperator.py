9ab7bc9d68aeaf134a06e2498d77d27efe558f3f
Aishwarya22222 <154801337+Aishwarya22222@users.noreply.github.com>
2023-12-30 12:56:15 +0545

    arithmaticoperator examples

---
+++ arithmeticoperator.py
@@ -0,0 +1,12 @@
+# +,_,/,*,%(mod->remainder),**(power),//(floor division)
+
+x=20
+y=10
+z=x+y
+print('the sum is',z)
+
+a=8//5
+print('the floor division is',a)
+
+b=47%6
+print('the modulus is',b)
