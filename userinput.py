5483a7274c519681e3340a06676539d815340d6f
Aishwarya22222 <154801337+Aishwarya22222@users.noreply.github.com>
2023-12-30 13:13:05 +0545

    assignment operator discussion

---
+++ userinput.py
@@ -0,0 +1,14 @@
+'''
+x=input('Enter the value of x: ')
+y=input('Enter the value of y: ')
+sum=x+y
+print('the sum is',sum)
+print(f'The sum is{sum}')
+'''
+
+
+x=int(input('Enter the value of x: '))
+y=int(input('Enter the value of y: '))
+sum=x+y
+print('the sum is',sum)
+print(f'The sum is {sum}')
