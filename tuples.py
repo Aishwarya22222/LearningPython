c6ca3eb67e2a029274892c7ddd293b071ff4ee86
Aishwarya22222 <154801337+Aishwarya22222@users.noreply.github.com>
2024-01-03 19:31:59 +0545

    knowldge on tuples

---
+++ tuples.py
@@ -0,0 +1,17 @@
+# tuple -> similar to list but immutable that is element inside tuple cannot be changed
+
+fruits=('banana','mango','lichhi','apple','orange','watermelon','apple')
+print(fruits)
+print(fruits[5])
+
+#len
+print(len(fruits)) 
+
+# basic tuple methods
+# .index() to enter the element and return the index number
+print(fruits.index('apple'))
+
+# .count() to count the number of times the element is appear in the tuple
+print(fruits.count('apple'))
+print(fruits.count('mango'))
+
