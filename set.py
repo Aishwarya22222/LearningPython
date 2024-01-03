c14f6f3ffd0e88f968012932e16e80aaa5355958
Aishwarya22222 <154801337+Aishwarya22222@users.noreply.github.com>
2024-01-03 20:27:29 +0545

    knowldge about set

---
+++ set.py
@@ -0,0 +1,15 @@
+# set are unordered collection of unique data
+# we can construct set using set() function
+
+xyz=set()
+# to add value use .add() method
+xyz.add(1)
+xyz.add(2)
+xyz.add(1)
+xyz.add(10)
+print(xyz)
+
+# for multiple data
+
+myset=set([10,58,64,79,5,8,97,74])
+print(myset)
