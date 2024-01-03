a88dc80404555d7378cd1781263e9b330f83d759
Aishwarya22222 <154801337+Aishwarya22222@users.noreply.github.com>
2024-01-03 19:12:39 +0545

    knowledge about list

--- lists.py
+++ lists.py
@@ -9,3 +9,30 @@ print(len(fruits))
 
 #indexing
 print(fruits[0])
+print(fruits[0][0])
+print(fruits[0][2].upper())
+print(fruits[1:4])
+
+# .append()-> to add new element in the list at the last position
+fruits.append('kiwi')
+print(fruits)
+
+# .pop()-> to remove element fro the list and by default it remove element of last position
+fruits.pop()
+print(fruits)
+
+# to remove from speecific position use indexing taht is .pop(1)
+fruits.pop(1)
+print(fruits)
+
+fruits.pop(0)
+print(fruits)
+
+# .sort()-> arrange in ascending order
+fruits.sort()
+print(fruits)
+
+# .reverse()-> reverse the array element
+fruits.reverse()
+print(fruits)
+
