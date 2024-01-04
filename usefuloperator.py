bf48c63c4e8a627b054963301f88706bee1bf872
Aishwarya22222 <154801337+Aishwarya22222@users.noreply.github.com>
2024-01-04 20:10:55 +0545

    min, max, shuffle, range, randint

---
+++ usefuloperator.py
@@ -0,0 +1,17 @@
+# min and max
+my_list=[10,5,20,28,50,35,40]
+print(max(my_list))
+print(min(my_list))
+
+#shuffle
+from random import shuffle
+shuffle(my_list)
+print(my_list)
+
+# for random integer use randint
+from random import randint
+print(randint(0,50))
+
+#range
+s=list(range(0,20,2))
+print(s)
