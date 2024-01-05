68dd33acd352f2e12047acd6d74157a3fa39bc5d
Aishwarya22222 <154801337+Aishwarya22222@users.noreply.github.com>
2024-01-05 23:27:55 +0545

    lambda modified

--- lambdaexpression.py
+++ lambdaexpression.py
@@ -15,7 +15,7 @@ mylist=[1,2,3,4,5,6,7,8,9,]
 result=list(filter(lambda num: num%2==0,mylist))
 print(result)
 
-
+# map wala ma lambda use garera
 numbers=[1,2,3,4,5,6,7,8,9,]
 a=list(map(lambda num: num**2,numbers))
 print(a)
