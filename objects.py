6a6716758b2b63a4f709db03f5d4e88d695f1b68
Aishwarya22222 <154801337+Aishwarya22222@users.noreply.github.com>
2024-01-08 14:45:23 +0545

    Objects->creating new object, instance of clss

---
+++ objects.py
@@ -0,0 +1,22 @@
+# Classes define the blueprint for objects, specifying the attributes (data) and methods (functions) 
+# that the objects will have. From class we can create instance, an instance is a specific object created
+# from the class.
+ 
+
+# print(type(12.5))
+# print(type([1,2,3,4,5]))
+
+# To create a new object
+
+class Student:
+    pass
+
+# instance of a class
+x=Student()
+print(type(x))
+
+y=Student()
+print(type(y))
+
+
+
